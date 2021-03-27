ea54e3713fc9ffdca45727b04aeba0f9bd2d1e04
atdykema <atdykema@gmail.com>
2021-03-27 00:27:25 -0400

    built first bit, added a color theme

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return (
+      <div className="Header">
+        <h1 class="top-left-title-1">placeholder1</h1>
+        <h1 class="top-left-title-2">placeholder2</h1>
+      </div>
+    );
+  }
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import './Hero.css';
+
+function Hero() {
+    return (
+      <div className="Hero">
+        
+      </div>
+    );
+  }
+
+export default Hero;
